55c56c1855a4d9832deb1eccbbddd02d7b5f6456
Facundo Carbone <162701255+Facundo-Martin-Carbone@users.noreply.github.com>
2024-05-03 09:54:30 -0300

    arreglo app

---
+++ App Login/ejemplo_flutter_app/lib/screens/login_screen.dart
@@ -0,0 +1,64 @@
+import 'package:ejemplo_flutter_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+// ignore: must_be_immutable
+class LoginScreen extends StatelessWidget 
+{
+  static const name = 'LoginScreen';
+  LoginScreen({super.key});
+
+  TextEditingController userController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  String user = 'Aiden';
+  String pass = '123';
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      body: Center
+      (
+        child: Column
+        (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: 
+          [
+            TextField
+            (
+              controller: userController,
+              decoration: const InputDecoration(hintText: 'Username', icon: Icon(Icons.person)),
+            ),
+            const SizedBox(height: 40,),
+            TextField
+            (
+              controller: passController,
+              decoration: const InputDecoration(hintText: 'Password', icon: Icon(Icons.lock),),
+              obscureText: true,
+            ),
+            const SizedBox(height: 50,),
+            ElevatedButton
+            (
+              onPressed: ()
+              {
+                String inputUser = userController.text;
+                String inputPass = passController.text;
+
+                if(inputUser == user && inputPass == pass)
+                {
+                  context.pushNamed(HomeScreen.name, extra: userController.text);
+                }
+                else if(userController.text != user || passController.text != pass)
+                {
+                  
+                }
+              }, 
+              child: const Text('Login')
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
